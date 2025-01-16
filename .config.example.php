f7a9c8f9d114e7c002b31df56f738446f98773f8
yrep <74735338+yrep@users.noreply.github.com>
2025-01-16 13:21:54 +0300

    Update .config.example.php
    Worker DB config

--- .config.example.php
+++ .config.example.php
@@ -3,3 +3,8 @@
 define('_DB_LOGIN_',  	'');
 define('_DB_PASSWORD_', '');
 define('_DB_DATABASE_', 'indiparser');
+
+define('_WDB_HOST_',  	'');
+define('_WDB_LOGIN_',  	'indiparser');
+define('_WDB_PASSWORD_', '');
+define('_WDB_DATABASE_', '');
